5186dbaaf4ef4a9f8dc6dd43bc02eba265967c72
florinrm <florinrmfm3@gmail.com>
2021-04-06 10:09:55 +0300

    Removed vmchecker line from lab 6

--- haskell/intro/solutii/intro-sol.hs
+++ haskell/intro/solutii/intro-sol.hs
@@ -448,4 +448,3 @@ check14 = tests_ 14
 Helpers for testing :) You can also run check1, check2 etc independently
 -}
 check = quickCheck False [check1, check2, check3, check4, check5, check6, check7, check8, check9, check10, check11, check12, check13, check14]
-vmchecker = vmCheck [check1, check2, check3, check4, check5, check6, check7, check8, check9, check10, check11, check12, check13, check14]
