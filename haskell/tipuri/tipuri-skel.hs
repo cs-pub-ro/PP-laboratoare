2d08c80d2f89329071464566a75032a1b5744275
Dana-Maria Nica <dana-maria.nica@ing.com>
2021-04-12 11:31:52 +0300

    Update skell exercises

--- haskell/tipuri/tipuri-skel.hs
+++ haskell/tipuri/tipuri-skel.hs
@@ -4,6 +4,7 @@
 
 import Data.List
 import Data.Maybe
+import Debug.Trace
 import TestPP
 
 {-
@@ -12,17 +13,20 @@ import TestPP
 
   Implementați următoarele operații cu vectori:
   - norma unui vector
+  - normalizarea unui vector
   - produsul scalar (dot product) dintre doi vectori
-  - adunarea a doi vectori
-  - scăderea a doi vectori
+  - verificarea ortogonalității unor vectori
 
   Explicații
 
   Fie a și b doi vectori din R^3 considerați de forma:
   a = a1 * i + a2 * j + a3 * k
   b = b1 * i + b2 * j + b3 * k
+  Produsul vectorial al celor doi vectori o să fie egal cu:
+  a x b = (a2 * b3 - a3 * b2) * i + (a3 * b1 - a1 * b3) * j + (a1 * b2 - a2 * b1) * k
   Produsul scalar al celor doi vectori o să fie egal cu:
   a • b = a1 * b1 + a2 * b2 + a3 * b3
+  Produsul scalar a doi vectori u și v este 0 dacă și numai dacă u și v sunt ortogonali.
 
   Pentru mai multe detalii, consultați:
   https://gerardnico.com/linear_algebra/vector_vector
@@ -36,14 +40,17 @@ data Vector = V
 lengthV :: Vector -> Double
 lengthV = undefined
 
+normalizeV :: Vector -> Vector
+normalizeV = undefined
+
 dotV :: Vector -> Vector -> Double
 dotV = undefined
 
-addV :: Vector -> Vector -> Vector
-addV = undefined
+computeDotProd :: [Vector] -> [Double]
+computeDotProd = undefined
 
-subV :: Vector -> Vector -> Vector
-subV = undefined
+orthogonalV :: [Vector] -> Bool
+orthogonalV = undefined
 
 check1 :: TestData
 check1 = let 
@@ -52,13 +59,171 @@ check1 = let
       v3 = V 0 0 0
   in tests_ 1 $
           [ testVal "lengthV" (sqrt 2) $ lengthV v1
+          , testVal "normalizeV" (V 0 (-1) 0) $ normalizeV (V 0 (-1) 0)
           , testVal "dotV" 0.0 $  dotV v1 v2
-          , testVal "addV" (V 2 0 0) $ addV v1 v2
-          , testVal "subV" v2 $ subV v2 v3
+          , testCond "orthogonalV" $ orthogonalV [v1, v2, v3]
           ]
 
 {-
- 2. Liste imbricate
+  2. Arbori binari de căutare
+
+  Definiți un tip de date BST a pentru a implementa un arbore binar de
+  căutare. Implementați funcții pentru a insera o valoare într-un 
+  arbore binar de căutare, căutarea unui element într-un arbore binar de 
+  căutare dat, o funcție care întoarce lista elementelor din parcurgerea
+  în inordine a arborelui. De asemenea, definiți funcționala analoagă lui 
+  map, care să aplice o funcție asupra cheilor nodurilor din arbore, și o 
+  funcțională analoagă lui foldl care să parcurgă nodurile în ordinea: rădăcină, 
+  nod_stanga, nod_dreapta ...
+
+-}
+
+data BST a = UndefinedNode | BSTNil deriving Show
+
+insertElem :: (Ord a, Eq a) => BST a -> a -> BST a
+insertElem = undefined
+
+findElem :: (Ord a, Eq a) => BST a -> a -> Maybe a
+findElem = undefined
+
+inorder :: BST a -> [a]
+inorder = undefined
+
+mapTree :: (a -> b) -> BST a -> BST b
+mapTree = undefined
+
+foldlTree :: (b -> a -> b) -> b -> BST a -> b
+foldlTree = undefined
+
+check2 :: TestData
+check2 = let root = foldl insertElem BSTNil [7, 4, 12, 2, 3, 1, 10, 15, 8]
+             values = [1, 2, 3, 4, 7, 8, 10, 12, 15]
+             foldValues = [15,8,10,12,3,1,2,4,7]
+             mapValues = map (*2) values
+             f = flip (:)
+  in tests_ 2 $ 
+          [ testVal "findElem" (Just 3) $ findElem root 3
+          , testVal "findElem" Nothing  $ findElem root 5
+          , testSet "inorder" values    $ inorder root
+          , testVal "mapTree" mapValues $ inorder $ mapTree (*2) root
+          , testVal "foldlTree (:)" foldValues $ foldlTree f [] root
+          , testVal "foldl (+)" 62 $ foldlTree (+) 0 root 
+          ]
+
+{-
+ 3. Structuri infinite - Arbore binar 
+
+  Pornind de la o valoare numerica x0, găsiți numărul minim de
+  aplicări de funcții succesive f sau g necesare pentru a ajunge 
+  la o valoare target xf.
+
+  De exemplu:
+    Fie f = \x -> 2 * x și g = \x -> 3 * x + 1
+    * pentru a ajunge la valoarea 8 din x0 = 1 este nevoie de 2 aplicări:
+      xf = 2 * 4 = f(4) = f(3 * 1 + 1) = g(1) = g(x0)
+    * pentru a ajunge la valoarea 13 din x0 = 1 este nevoie de 2 aplicări:
+      xf = 3 * 4 + 1 = g(4) = f(3 * 1 + 1) = g(1) = g(x0)
+    * de la x0 = 1 la xf = 10 nu putem ajunge cu ajutorul funcților anterior
+      mentionate
+
+    De ce ne ajută o structură arborescentă în acest caz?
+
+    Putem construi un arbore binar infinit avand ca radacină un nod cu 
+    valoarea x0. Pentru construirea nodului de pe ramura din stânga se 
+    va aplica funcția f, iar pe ramura din dreapta se va aplica funcția g.
+
+    Exemplu:
+                               ┌─────┐
+               ┌───────────────┤x0=1 ├────────────────┐
+               │               └─────┘                │
+               │                                      │
+               │                                      │
+           ┌───┴───┐                              ┌───┴───┐
+         ┌─┤f(1)=2 ├───────────┐                 ┌┤g(1)=4 ├┐
+         │ └───────┘           │                 │└───────┘│
+         │                     │                 │         │
+         │                     │                 │         │
+     ┌───┴───┐             ┌───┴────┐        ┌───┴───┐ ┌───┴────┐
+    ┌┤f(2)=4 ├┐           ┌┤g(2)=7  ├─┐      │f(4)=8 │ │g(4)=13 │
+    │└───────┘│           │└────────┘ │      └───────┘ └────────┘
+    │         │           │           │          │         │ 
+    │         │           │           │        .....     .....
+┌───┴───┐ ┌───┴────┐ ┌────┴────┐ ┌────┴────┐
+│f(4)=8 │ │g(4)=13 │ │f(7)=14  │ │g(7)=22  │   
+└───────┘ └────────┘ └─────────┘ └─────────┘
+    │         │           │           │
+  .....     .....       .....       .....
+
+  Problema se transformă în găsirea unui drum minim între nodul radacină 
+  și un nod dat.
+
+  Astfel, extindeți tipul definit anterior (de exemplu puteți adăuga un 
+  câmp parinte, un câmp string pentru a reține funcția aplicată pe nodul curent) 
+  și implementați următoarele funcții:
+    * completeBinaryTree - pornind de la x0 construiește arborele binar infinit aplicând
+      f pe nodul stâng, respectiv g pe nodul drept
+    * bfs - primește un arbore și întoarce parcurgerea bfs a acestuia - o lista infinită 
+      de noduri, care vor fi expandate într-o listă de noduri copil (stanga, dreapta)
+    * extractPath - primește un nod și întoarce calea către rădacină, o listă de perechi
+      de forma (valoare, funcție_aplicată) 
+    * path - primește 2 numere x0 si xf și întoarce calea de la x0 la xf o listă de perechi
+      de forma (valoare, funcție_aplicată). Întoarce o listă vidă în cazul în care nu se 
+      poate obține xf cu ajutorul funcților date.
+
+  Dacă aveți pe nodul stâng și nodul drept doar funcții monoton crescatoare, cum puteți
+  opri căutarea?
+
+  Similar exercițiului 9 din laboratorul anterior experimentați în consolă funcționalitățile 
+  funcției "trace" în definirea nodurilor din funția completeBinaryTree. Ce observați?
+  
+-}
+
+data InfBST a = UndefinedTree deriving Show
+
+f :: (Num a) => a -> a
+f = \x -> 2 * x 
+
+g :: (Num a) => a -> a
+g = \x -> 3 * x + 1
+
+completeBinaryTree :: (Show a, Num a) => a -> InfBST a
+completeBinaryTree = undefined
+
+bfs :: (Num a) => InfBST a -> [InfBST a]
+bfs = undefined
+
+extractPath :: (Num a, Show a) => InfBST a -> [(a, String)]
+extractPath = undefined
+
+stopCond :: (Num a, Ord a) => a -> a -> Bool
+stopCond = undefined
+
+path :: (Ord a, Num a, Show a) => a -> a -> [(a, String)]
+path = undefined
+
+check3 :: TestData
+check3 = let bfsNodes = bfs $ completeBinaryTree 1
+             values = [1, 2, 4, 4, 7, 8, 13, 8, 13, 14]
+  in tests_ 3 $ 
+          [ testVal "bfs" values (value <$> take 10 bfsNodes)
+          , testVal "path 1 8" [(1,"g"),(4,"f")] $ path 1 8
+          , testVal "path 1 100" [(1,"g"),(4,"f"),(8,"g"),(25,"f"),(50,"f")] $ path 1 100
+          , testVal "path 1 16" [(1,"g"),(4,"f"),(8,"f")] $ path 1 16
+          , testVal "path 1 10" [] $ path 1 10
+          ]  
+          
+{-
+ 4. Cum ați reprezenta un arbore oarecare?
+ 
+ Exercițiu testat manual de asistent
+-}
+data Tree a = TreeNode deriving Show
+
+check4 :: TestData
+check4 = tests_ 4 $ [testManually "General Tree" False]
+
+{-
+ 5. Liste imbricate
 
   Definiți un tip de date SList a care să aibă funcționalități
   asemănătoare listelor din limbajele Lisp (e.g. Scheme, Racket, Clojure),
@@ -102,13 +267,13 @@ deepEqual = undefined
 flatten :: SList a -> [a]
 flatten = undefined
 
-check2 :: TestData
-check2 = let l1 = consElem 1 $ emptySList
+check5 :: TestData
+check5 = let l1 = consElem 1 $ emptySList
              l2 = consElem 2 $ consList (consElem 1 $ consElem 1 emptySList) $
                   consElem 3 emptySList
              l3 = consList (consElem 1 $ consElem 1 emptySList) $ consElem 3 $
                   emptySList
-  in tests_ 2 $
+  in tests_ 5 $
           [ testCond "simple lists1" $ deepEqual l1 l1
           , testCond "simple lists 2 " $ not (deepEqual l1 l2)
           , testCond "less simple lists" $ deepEqual (consElem 2 $ l3) l2
@@ -118,74 +283,7 @@ check2 = let l1 = consElem 1 $ emptySList
           ]
 
 {-
-  3. Arbori binari de căutare
-  Definiți un tip de date BST a pentru a implementa un arbore binar de
-  căutare. De asemenea, definiți funcții pentru a insera o valoare într-un 
-  arbore binar de căutare, căutarea unui element într-un arbore binar de 
-  căutare dat, o funcție care întoarce lista elementelor din parcurgerea
-  în inordine a arborelui și o funcție care întoarce cel mai mic subarbore
-  care conține două noduri ale căror chei sunt date.
--}
-data BST a = UndefinedNode | BSTNil deriving Show
-
-insertElem :: (Ord a, Eq a) => BST a -> a -> BST a
-insertElem = undefined
-
-findElem :: (Ord a, Eq a) => BST a -> a -> Maybe a
-findElem = undefined
-
-subTree :: (Ord a, Eq a) => BST a -> a -> a -> Maybe (BST a)
-subTree = undefined
-
-inorder :: BST a -> [a]
-inorder = undefined
-
-check3 :: TestData
-check3 = let root = foldl insertElem BSTNil [7, 4, 12, 2, 3, 1, 10, 15, 8]
-             values = [1, 2, 3, 4, 7, 8, 10, 12, 15]
-             tree1 = subTree root 1 3 
-             tree2 = subTree root 4 5
-             tree3 = subTree root 7 15
-             tree4 = subTree root 1 8
-  in tests_ 3 $ 
-          [ testVal "findElem" (Just 3) $ findElem root 3
-          , testVal "findElem" Nothing  $ findElem root 5
-          , testSet "inorder" values    $ inorder root
-          , testSet "subTree" [1, 2, 3] $ inorder (fromJust tree1)
-          , testCond "subTree"          $ isNothing tree2
-          , testSet "subTree" values    $ inorder (fromJust tree3)
-          , testSet "subTree" values    $ inorder (fromJust tree4)
-          ]
-
-{-
-  4. Arbore multicăi nevid
-  Având dat tipul Tree a, definiți funcționala analoagă lui map, care să
-  aplice o funcție asupra cheilor nodurilor din arbore, și o funcțională
-  analoagă lui foldl care să parcurgă nodurile în ordinea: rădăcină, copil_1,
-  copil_2, ... copil_n. 
--}
-
-data Tree a = Node
-  { value :: a
-  , children :: [Tree a]
-  } deriving (Eq, Show)
-
-mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree = undefined
-
-foldlTree :: (b -> a -> b) -> b -> Tree a -> b
-foldlTree = undefined
-
-check4 :: TestData
-check4 = let tree = Node 1 [Node 2 [Node 3 []], Node 4 [Node 5 [], Node 6 [Node 7 [], Node 8 []]]]
-  in tests_ 4 $
-          [ testSet "foldlTree" [1..8] $ foldlTree (flip (:)) [] tree
-          , testVal "foldlTree" 36     $ foldlTree (+) 0 tree
-          , testVal "mapTree"   72     $ foldlTree (+) 0 $ mapTree (*2) tree
-          ]
-
-{-
-  5. Difference lists
+  6. Difference lists
   Se cere definirea tipului de date "difference list".
 
   Un difference list este o listă "parțial construită", i.e. ale cărei
@@ -253,8 +351,8 @@ headDL = undefined
 tailDL :: DList a -> Maybe (DList a)
 tailDL = undefined
 
-check5 :: TestData
-check5 = tests_ 5 $
+check6 :: TestData
+check6 = tests_ 6 $
           [ testVal "toList, fromList" "Ana are mere" $ dlToList (dlFromList "Ana are mere")
           , testVal "cons, empty" [1,2,3] $ dlToList $ consDL 1 $ consDL 2 $ consDL 3 emptyDL
           , testVal "snoc, empty" [3,2,1] $ dlToList $ snocDL 1 $ snocDL 2 $ snocDL 3 emptyDL
@@ -265,6 +363,6 @@ check5 = tests_ 5 $
           ]
 
 {-
-Helpers for testing :) You can also run check1, check2 etc independently
+ Helpers for testing :) You can also run check1, check2 etc independently
 -}
-check = quickCheck False [check1, check2, check3, check4, check5]
+check = quickCheck False [check1, check2, check3, check4, check5, check6]
