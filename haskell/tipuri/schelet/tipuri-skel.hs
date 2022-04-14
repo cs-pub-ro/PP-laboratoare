7d16e7531c5b1bc9e8e7b11df8379e3781da9ebb
IonutBalasescu <44617121+IonutBalasescu@users.noreply.github.com>
2022-04-15 00:17:19 +0300

    Update haskell/tipuri/schelet/tipuri-skel.hs
    Co-authored-by: Florin Mihalache <florinrm@users.noreply.github.com>

--- haskell/tipuri/schelet/tipuri-skel.hs
+++ haskell/tipuri/schelet/tipuri-skel.hs
@@ -182,7 +182,8 @@ check4 = let l1 = consElem 1 $ emptyList
 
   Aveți mai jos rezolvată și explicată o problemă deosebită de
 
-  generare de arbori infiniti. Gasiti si pasii de rezolvare in
+  generare de arbori infiniți. Găsiți și pașii de rezolvare în
+
   inregistrarea de anul trecut aici:
   https://ctipub-my.sharepoint.com/:v:/g/personal/mihnea_muraru_upb_ro/EcYo_vC9AktDt0fmeIqcdtEBZLsqbf4X6buhe9Gru8ocIg
 
