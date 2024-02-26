34f250dc8fa61a47dbaa0564fa5072749b236376
oornot <79811463+oornot@users.noreply.github.com>
2024-02-26 13:42:51 +0200

    Add files via upload

--- racket/intro/solutii/palindromes-sol.rkt
+++ racket/intro/solutii/palindromes-sol.rkt
@@ -12,9 +12,9 @@
 (define (check-set given expected) (check-set-part  "" 1 given expected)) (define (check-set-part part per given expected) (check-test part per (λ (x y) (apply equal? (map list->seteqv `(,given ,expected)))) given expected "nu este echivalent cu cel așteptat"))
 (define (check-set-unique given expected) (check-set-unique-part  "" 1 given expected)) (define (check-set-unique-part part per given expected) (check-test part per (λ (x y) (and (apply = (map length `(,given ,expected))) (apply equal? (map list->seteqv `(,given ,expected))))) given expected "nu este echivalent cu cel așteptat"))
 (define (check-test part percent tester given expected err-m) (if (not (tester given expected)) (and (when (member given default-results) (set! defaults (cons (if (< percent 1) (cons n-ex part) n-ex) defaults)))
-  (when (or (not (member given default-results)) (<= (length defaults) show-defaults))
-    (printf "[~a][--]~a rezultatul ~v ~a: ~v~n" (p-n-ex) (if (< percent 1) (format " la ~a ~v" (car name-ex) part) "") given err-m expected)))
- (let ((pts (* p-ex percent))) (printf (string-join (list "~a ~a ~a~a~a" (if nopoints "~a~a" ": +~v ~a") "~n") "") (if (and prepend (not nopoints)) (format "+~v:" pts) (format "[~a][OK]" (p-n-ex))) (car name-ex) (if (or prepend nopoints) n-ex "") (if (< percent 1) (format "~v " part) " ") (caddr name-ex) (if nopoints "" pts) (if nopoints "" (if (= pts 1) 'punct 'puncte))) (set! total (+ total pts)))))
+                                                                                                     (when (or (not (member given default-results)) (<= (length defaults) show-defaults))
+                                                                                                       (printf "[~a][--]~a rezultatul ~v ~a: ~v~n" (p-n-ex) (if (< percent 1) (format " la ~a ~v" (car name-ex) part) "") given err-m expected)))
+                                                                  (let ((pts (* p-ex percent))) (printf (string-join (list "~a ~a ~a~a~a" (if nopoints "~a~a" ": +~v ~a") "~n") "") (if (and prepend (not nopoints)) (format "+~v:" pts) (format "[~a][OK]" (p-n-ex))) (car name-ex) (if (or prepend nopoints) n-ex "") (if (< percent 1) (format "~v " part) " ") (caddr name-ex) (if nopoints "" pts) (if nopoints "" (if (= pts 1) 'punct 'puncte))) (set! total (+ total pts)))))
 (define (sumar) (when (and (not (null? defaults)) (< show-defaults (length defaults))) (p `(... rezultatul implicit dat la ,(cadr name-ex) ,(reverse defaults)))) (when (not nopoints) (printf "total: ~v puncte~n" total)))
 
 
@@ -30,6 +30,8 @@
 (exercițiul 1 : 1 punct)
 ;; Fie următoarele axiome pentru obținerea reprezentării unui număr natural
 ;; în baza b (cu [] = lista vidă și ++ = concatenare).
+;; Convenim ca numărul 0 (în orice bază) să fie reprezentat prin lista vidă,
+;; în rest reprezentarea corespunde reprezentării matematice.
 ;; num->base(0,b) = [ ]                                   ; pt n=0
 ;; num->base(n,b) = num->base(n div b, b) ++ [ n mod b ]  ; pt n>0
 ;; Implementați funcția corespunzătoare în Racket.
@@ -38,8 +40,8 @@
   (if (zero? n)
       '()
       (append (num->base (quotient n b) b)
-              (list (modulo n b))))) ; este necesar sa facem lista din n mod b, intrucat 
-                                     ; append primeste ca argumente obligatoriu liste
+              (list (modulo n b))))) ; este necesar să facem listă din n mod b, întrucât 
+                                     ; append primește ca argumente obligatoriu liste
 
 (check-exp-part 'a .5 (num->base 489 10) '(4 8 9))
 (check-exp-part 'b .5 (num->base 489 2) '(1 1 1 1 0 1 0 0 1))
@@ -57,30 +59,28 @@
       (append (rev (cdr L))
               (list (car L)))))
 
-(check-exp (rev '(5 1 4 8 7)) '(7 8 4 1 5))
+(check-exp-part 'a .5 (rev '(5 1 4 8 7)) '(7 8 4 1 5))
+(check-exp-part 'b .5 (rev '(a b c a)) '(a c b a))
 
 
-(exercițiul 3 : 2 puncte)
+(exercițiul 3 : 1 punct)
 ;; Un reverse care acționează și asupra elementelor din eventualele liste interioare.
 ;; ex: Pt lista L = '(1 b (3 d (5 f)) (7 (h (9 10) 11)) 12):
 ;; (rev L) întoarce '(12 (7 (h (9 10) 11)) (3 d (5 f)) b 1)
 ;; (deep-rev L) ->  '(12 ((11 (10 9) h) 7) ((f 5) d 3) b 1)
 
-; cazul 1) - cazul de bază
+; Cazul 1) - cazul de bază:
 ;; deep-rev([ ]) = [ ]
 
-; cazul 2) - când elementul curent NU este o listă, se procedează la fel ca la funcția rev de mai sus
+; Cazul 2) - când elementul curent NU este o listă, procedăm ca la funcția rev de mai sus:
 ;; deep-rev(x:l) = deep-rev(l) ++ [x]
 
-; cazul 3) - se procedează similar ca la cazul 2),
-; însă aici avem cazul cand elementul curent este, de asemenea o listă,
-; acesta trebuie inversat la rândul lui,
-; astfel, trebuie apelat deep-rev atât pe elementul curent, cât și pe restul listei
-;; deep-rev((x:xl):l) = deep-rev(l) ++ [deep-rev(x:xl)]
-                  
+; Cazul 3) - similar, dar când elementul curent este de asemenea o listă,
+; această listă trebuie și ea inversată (cu tot cu eventualele liste interioare).
+; În acest scop folosim tot deep-rev:
+;; deep-rev(L:l) = deep-rev(l) ++ [deep-rev(L)]
 
 ;; Restricții:
-;; - se va implementa obligatoriu recursiv, după modelul implementării lui rev
 ;; - folosiți cond
 ;; - folosiți list? pentru a verifica dacă elementul procesat este o listă
 
@@ -95,6 +95,10 @@
 
 (exercițiul 4 : 1 punct)
 ;; Implementați testul că o listă L este palindrom.
+;; Abordarea recomandată este să descrieți CE este un palindrom,
+;; mai degrabă decât CUM se verifică proprietatea de palindrom.
+;; Genul acesta de abordare corespunde stilului declarativ de
+;; a programa (în contrast cu stilul procedural).
 
 (define (palindrome? L)
   (equal? L (rev L)))
@@ -105,21 +109,7 @@
 (check-exp-part 'd .25 (palindrome? '()) #t)
 
 
-(exercițiul 5 : 1 punct)
-;; Pentru o listă de liste de elemente, determinați dacă listele interioare
-;; sunt toate palindroame.
-
-;; Restricții:
-;; - folosiți doar rev și/sau deep-rev, nu recursivitate
-
-(define (palindrome-list? L)
-  (equal? (rev L) (deep-rev L)))
-
-(check-exp-part 'a .5 (palindrome-list? '((2 a 2) (b) (3) (a a 1 1 a a))) #t)
-(check-exp-part 'b .5 (palindrome-list? '((a a 1 a a) (4 0) (a a 1 a a))) #f)
-
-
-(exercițiul 6 : 2 puncte)
+(exercițiul 5 : 2 puncte)
 ;; Testați că n este palindrom în toate bazele din lista Bases.
 
 ;; a) După ce variabilă/variabile e util să faceți recursivitatea?
@@ -131,7 +121,7 @@
 ;;      (respectați mereu tipul întors de funcție: trebuie să fie număr, listă, bool, etc.?)
 ;;    - gândiți-vă cum valorile întoarse pe cazul de bază sunt preluate de funcție pentru a calcula
 ;;      rezultatul final
-;; c) Care este apelul recursiv (e obligatoriu să aveți minim unul), și care este formula prin care 
+;; c) Care este apelul recursiv (e obligatoriu să aveți minim unul), și care este formula prin care
 ;;    din apelul recursiv se obține rezultatul final?
 
 (define (all-palindromes? n Bases)
@@ -145,8 +135,8 @@
 (check-exp-part 'b .5 (all-palindromes? 594 '(2 10)) #f)
 
 
-(exercițiul 6.0 : 0 puncte)
-;; Dacă nu ați făcut-o deja, încercați să înlocuiți if sau cond de mai sus cu o combinație 
+(exercițiul 6 : 0 puncte)
+;; Dacă nu ați făcut-o deja, încercați să înlocuiți if sau cond de mai sus cu o combinație
 ;; de operatori logici (and, or, not).
 ;; Apoi testați-vă singuri funcția (fără check-expect, doar rulați-o pe o listă aleasă de voi).
 
@@ -155,13 +145,16 @@
       (and (palindrome? (num->base n (car Bases)))
            (all-pals? n (cdr Bases)))))
 
+
 (exercițiul 7 : 2 puncte)
 ;; Găsiți toate numerele naturale, mai mici sau egale cu n, care sunt
 ;; palindroame în toate bazele din lista Bases.
 
 ;; Sugestii:
 ;; - folosiți funcția anterioară.
-;; - efectuați pașii a, b, c descriși mai sus.
+;; - efectuați pașii a, b, c descriși mai sus:
+;;   a) Vrem să facem recursivitate după n sau după Bases?
+;;   ... etc.
 
 (define (palindromes-to-n n Bases)
   (cond 
@@ -176,9 +169,9 @@
 (check-exp (palindromes-to-n 100 '(2 10)) '(0 1 3 5 7 9 33 99))
 
 
-(exercițiul 8 : 2 puncte BONUS)
-;; Să se găsească primul număr mai mare decât start care este palindrom în
-;; minim b baze dintre bazele 2, 3, 4, 5, 6, 7, 8, 9, 10.
+(exercițiul 8 : 2 puncte)
+;; Să se găsească primul număr mai mare sau egal cu start care este 
+;; palindrom în minim b baze dintre bazele 2, 3, 4, 5, 6, 7, 8, 9, 10.
 
 ;; Sugestii:
 ;; - definiți-vă una sau mai multe funcții ajutătoare
@@ -186,41 +179,41 @@
 ;;   - scrieți funcția finală apelând oricâte funcții ajutătoare încă inexistente
 ;;   - apoi implementați toate funcțiile de care ați descoperit că aveți nevoie
 
-(define (count-bases n L)
+(define (count-bases n Bases)
   (cond
-    ((null? L) 0)
-    ((palindrome? (num->base n (car L))) (add1 (count-bases n (cdr L))))
-    (else (count-bases n (cdr L)))
-    ))
+    ((null? Bases) 0)
+    ((palindrome? (num->base n (car Bases))) (add1 (count-bases n (cdr Bases))))
+    (else (count-bases n (cdr Bases)))))
       
-
 (define (first-b-pal start b)
   (if (>= (count-bases start '(2 3 4 5 6 7 8 9 10)) b)
       start
-      (first-b-pal (add1 start) b)
-  ))
+      (first-b-pal (add1 start) b)))
 
 (check-exp-part 'a .5 (first-b-pal 10 4) 121)
-(check-exp-part 'b .5 (first-b-pal 150 4) 373)
+(check-exp-part 'b .5 (first-b-pal 154 4) 373)
+(check-exp-part 'c .5 (first-b-pal 154 3) 154)
 
 
-(exercițiul 9 : 3 puncte BONUS)
+(exercițiul 9 : 3 puncte)
 ;; Să se găsească cea mai lungă porțiune continuă a unui număr care este palindrom.
-;; Dacă există mai multe asemenea porțiuni, întoarceți una (oarecare) dintre ele. 
+;; Dacă există mai multe asemenea porțiuni, întoarceți una (oarecare) dintre ele.
 
 ;; Funcție ajutătoare care transformă o listă de cifre în număr
 (define (list->num L) (foldl (lambda (dig num) (+ dig (* 10 num))) 0 L))
 
 ;; Sugestii:
+;; - convertiți numerele în liste pentru a le prelucra cu funcțiile pe liste
+;;   (pentru conversia inversă este dată funcția list->num de mai sus)
 ;; - folosiți take - (take L n) întoarce prefixul de lungime n al listei L
 
 (define (longest-palindrome n)
   (define L (num->base n 10)) ;; numărul ca listă
   (define len (length L))     ;; lungimea listei (câte cifre are numărul)
   (define (iter L len crtlen) ;; caută în L palindroame de lungime len (crtlen = lungime curentă L) ;; întoarce palindromul dacă există, altfel #f
-    (and (<= len crtlen)                              ;; observație: (and #t 5) întoarce 5 ; (or #f 4) întoarce 4 (se putea si cu if-uri)
+    (and (<= len crtlen)                              ;; observație: (and #t 5) întoarce 5 ; (or #f 4) întoarce 4 (se putea și cu if-uri)
          (if (palindrome? (take L len))
-             (list->num (take L len))
+             (list->num (take L len))                 ;; întoarcem palindromul ca număr, nu ca listă
              (iter (cdr L) len (sub1 crtlen)))))
   (define (iter2 leng)        ;; încearcă să găsească palindroame, în ordinea descrescătoare a lungimii leng
     (if (= leng 1)
