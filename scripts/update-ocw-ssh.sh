3679e44f1ccb7bf266fee1a4e1520fb3981188bd
Luca Mazilescu <luca.mazilescu@protonmail.com>
2026-04-08 08:56:30 +0300

    CI: omit empty upload list
    Signed-off-by: Luca Mazilescu <luca.mazilescu@protonmail.com>

--- scripts/update-ocw-ssh.sh
+++ scripts/update-ocw-ssh.sh
@@ -4,4 +4,6 @@ python -m pip install pypandoc
 python -c "from pypandoc.pandoc_download import download_pandoc;download_pandoc(version='2.5')"
 
 files=`git diff --name-only HEAD~1 HEAD | grep README.md`
-python scripts/publish_ssh.py --files $files
+if [[ $files ]]; then
+    python scripts/publish_ssh.py --files $files
+fi
